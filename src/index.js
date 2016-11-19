9663bc28aff5003bdbeb1d2db37477ae6e7e2c70
Alexander Prime <rainesabaal@gmail.com>
2016-11-19 07:54:37 -0700

    Stop trying to download a document we aren't using

--- src/index.js
+++ src/index.js
@@ -12,8 +12,3 @@ ReactDOM.render(
 	<Provider store={ store }><Editor /></Provider>,
 	document.getElementById( 'root' ),
 );
-
-
-fetch( 'http://localhost:8081/documents' )
-	.then( res => res.json() )
-	.then( docs => { /* dispatch here */ } );
