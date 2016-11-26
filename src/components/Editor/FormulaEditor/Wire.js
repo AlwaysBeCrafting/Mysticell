84c774a254daf047a533738066acbdf902e04b38
Alexander Prime <rainesabaal@gmail.com>
2016-11-25 17:35:41 -0700

    Wrap wire SVG in a div so we can size it correctly

--- src/components/Editor/FormulaEditor/Wire.js
+++ src/components/Editor/FormulaEditor/Wire.js
@@ -4,9 +4,11 @@ import './Wire.less';
 
 
 
-const Wire = props => <svg className="wire" viewBox="0 0 1 1" preserveAspectRatio="none">
-	<path d="M 0,0 C 0.5,0 0.5,1 1,1" vectorEffect="non-scaling-stroke" />
-</svg>;
+const Wire = props => <div className="wire">
+	<svg viewBox="0 0 1 1" preserveAspectRatio="none">
+		<path d="M 0,0 C 0.5,0 0.5,1 1,1" vectorEffect="non-scaling-stroke" />
+	</svg>
+</div>;
 
 
 
