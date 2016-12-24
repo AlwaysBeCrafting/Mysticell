b785031aeaede5be043fd7081901af14c318a0c5
Alexander Prime <rainesabaal@gmail.com>
2016-12-24 00:27:08 -0700

    Create load-document action/reducer

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -3,11 +3,18 @@ import { Position } from 'data/shared';
 import { AddNodeAction } from './addNode';
 import { CollapseFieldAction } from './collapseField';
 import { ExpandFieldAction } from './expandField';
+import { LoadDocumentAction } from './loadDocument';
 import { MoveNodeAction } from './moveNode';
 import { SetPathAction } from './setPath';
 import { SetTitleAction } from './setTitle';
 
-export type Action = AddNodeAction  | CollapseFieldAction | ExpandFieldAction |
-					MoveNodeAction | SetPathAction | SetTitleAction;
+export type Action =
+	AddNodeAction |
+	CollapseFieldAction |
+	ExpandFieldAction |
+	LoadDocumentAction |
+	MoveNodeAction |
+	SetPathAction |
+	SetTitleAction;
 
 export default Action;
