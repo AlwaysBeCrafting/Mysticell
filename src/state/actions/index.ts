85d693af4be2bd2aff939debe60c6190932a944d
Alexander Prime <rainesabaal@gmail.com>
2016-12-23 22:13:16 -0700

    Remove action interfaces, import instead

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -2,22 +2,10 @@ import { Position } from 'data/shared';
 
 import { AddNodeAction } from './addNode';
 import { CollapseFieldAction } from './collapseField';
+import { ExpandFieldAction } from './expandField';
 import { MoveNodeAction } from './moveNode';
-
-export interface ExpandFieldAction {
-	type: 'EXPAND_FIELD';
-	fieldId: number;
-}
-
-export interface SetPathAction {
-	type: 'SET_PATH';
-	path: string[];
-}
-
-export interface SetTitleAction {
-	type: 'SET_TITLE';
-	title: string;
-}
+import { SetPathAction } from './setPath';
+import { SetTitleAction } from './setTitle';
 
 export type Action = AddNodeAction  | CollapseFieldAction | ExpandFieldAction |
 					MoveNodeAction | SetPathAction | SetTitleAction;
