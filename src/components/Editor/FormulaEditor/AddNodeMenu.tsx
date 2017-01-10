bdb98a6c70711a6b3700778fdb8e0327b5c6f477
Alexander Prime <rainesabaal@gmail.com>
2017-01-10 16:55:06 -0700

    Style menu items nicely

--- src/components/Editor/FormulaEditor/AddNodeMenu.tsx
+++ src/components/Editor/FormulaEditor/AddNodeMenu.tsx
@@ -6,15 +6,21 @@ import { Fxn, fxnList } from 'data/fxn';
 import { NodeState } from 'state';
 import Action from 'state/action';
 
+import './AddNodeMenu.less';
+
 interface AddNodeMenuDispatch {
 	dispatch: ( action: Action ) => void;
 }
 
-const AddNodeMenu = ( props: AddNodeMenuDispatch ) => <ul> {
-	fxnList.map( fxn =>
-		<li>{ fxn.name }</li>,
-	)
-} </ul>;
+const AddNodeMenu = ( props: AddNodeMenuDispatch ) => (
+	<ul className="add-node-menu">
+		{
+			fxnList.map( fxn =>
+				<li>{ fxn.name }</li>,
+			)
+		}
+	</ul>
+);
 
 export default reduxConnect(
 	() => ({}),
