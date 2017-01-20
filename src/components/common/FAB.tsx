0f633f0cbfb7c66b775b7a48b5663f7e49fe25bb
Alexander Prime <rainesabaal@gmail.com>
2017-01-19 21:03:26 -0700

    Fix remaining single-quoted strings

--- src/components/common/FAB.tsx
+++ src/components/common/FAB.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
+import * as React from "react";
 
-import './FAB.less';
+import "./FAB.less";
 
 export interface FabProps {
 	icon: string;
--- src/data/fxn.ts
+++ src/data/fxn.ts
@@ -6,44 +6,44 @@ export interface Fxn {
 }
 
 export const ADD: Fxn = {
-	name:   'Add',
-	inputNames: [ 'A', 'B' ],
-	outputName: 'Sum',
+	name:   "Add",
+	inputNames: [ "A", "B" ],
+	outputName: "Sum",
 	exec:   ( a: number, b: number ) => +a + +b,
 };
 
 export const SUBTRACT: Fxn = {
-	name:   'Subtract',
-	inputNames: [ 'A', 'B' ],
-	outputName: 'Difference',
+	name:   "Subtract",
+	inputNames: [ "A", "B" ],
+	outputName: "Difference",
 	exec:   ( a: number, b: number ) => +a - +b,
 };
 
 export const MULTIPLY: Fxn = {
-	name:   'Multiply',
-	inputNames: [ 'A', 'B' ],
-	outputName: 'Product',
+	name:   "Multiply",
+	inputNames: [ "A", "B" ],
+	outputName: "Product",
 	exec:   ( a: number, b: number ) => +a * +b,
 };
 
 export const DIVIDE: Fxn = {
-	name:   'Divide',
-	inputNames: [ 'A', 'B' ],
-	outputName: 'Quotient',
+	name:   "Divide",
+	inputNames: [ "A", "B" ],
+	outputName: "Quotient",
 	exec:   ( a: number, b: number ) => +a / +b,
 };
 
 export const SQRT: Fxn = {
-	name:   'Square Root',
-	inputNames: [ 'Number' ],
-	outputName: 'Square root',
+	name:   "Square Root",
+	inputNames: [ "Number" ],
+	outputName: "Square root",
 	exec:   Math.sqrt,
 };
 
 export const POWER: Fxn = {
-	name:   'Power',
-	inputNames: [ 'Number', 'Exponent' ],
-	outputName: 'Result',
+	name:   "Power",
+	inputNames: [ "Number", "Exponent" ],
+	outputName: "Result",
 	exec:   ( a: number, b: number ) => a ** +b,
 };
 
