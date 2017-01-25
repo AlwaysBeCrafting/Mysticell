8929588b1320dfd521b87fb123f23ae63035db00
Alexander Prime <rainesabaal@gmail.com>
2017-01-25 16:30:34 -0700

    Handle zeroes as numeric, cast input types nicely

--- src/redux/reducers/nodes.ts
+++ src/redux/reducers/nodes.ts
@@ -53,31 +53,37 @@ export const reducer = (
 				...action.payload.node,
 				inputValues: action.payload.node.inputValues,
 			};
-			updatedNode.inputValues[action.payload.inputIndex] = action.payload.inputValue;
-			return updateNodeAndDependents( state, updatedNode );
-
+			const inVal = action.payload.inputValue;
+			updatedNode.inputValues[action.payload.inputIndex] = castInputValue( inVal );
+			const returnVal = updateNodeAndDependents( state, updatedNode );
+			return returnVal;
 
 		default: return state;
 	}
+};
+
+const castInputValue = ( value ): number|string|undefined => {
+	if ( value === "" ) { return ""; }
+	if ( !Number.isNaN( +value )) { return +value; }
+	return value;
 };
 
 const updateNodeAndDependents = ( nodes: Map<number, NodeState>, currentNode: NodeState ): Map<number, NodeState> => {
 	const inputValues = currentNode.inputNodes
 		.map(( nodeId, i ) => {
 			const inputNode = nodeId && nodes.get( nodeId );
-			return ( inputNode && inputNode.outputValue ) || currentNode.inputValues[i];
+			return inputNode ? inputNode.outputValue : currentNode.inputValues[i];
 		});
 
 	const outputValue = currentNode.fxn.exec( ...inputValues );
 
-	return getConnectedNodes( nodes, currentNode )
-		.reduce(
+	return getConnectedNodes( nodes, currentNode ).reduce(
 			updateNodeAndDependents,
 			new Map( nodes ).set( currentNode.id, { ...currentNode, inputValues, outputValue }),
 		);
 };
 
-const getConnectedNodes = ( nodes: Map<number, NodeState>, currentNode: NodeState ) => {
+const getConnectedNodes = ( nodes: Map<number, NodeState>, currentNode: NodeState ): NodeState[] => {
 	return Array.from( nodes )
 		.filter(([ id, node ]) => node.inputNodes.find( inputId => !!inputId && ( inputId === currentNode.id )))
 		.map(([ id, node ]) => node );
