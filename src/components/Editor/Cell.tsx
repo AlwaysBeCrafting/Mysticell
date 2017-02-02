fd13a5c52bbbaedf76c85474709d613f5b887815
Alexander Prime <rainesabaal@gmail.com>
2017-02-01 17:15:02 -0700

    Display a field's resultNode's outputValue, for now

--- src/components/Editor/Cell.tsx
+++ src/components/Editor/Cell.tsx
@@ -1,14 +1,22 @@
 import * as React from "react";
+import { connect as reduxConnect } from "react-redux";
 
-import { CellState } from "redux/state";
+import { CellState, FieldState, NodeState } from "redux/state";
 
 import "./Cell.less";
 
-interface CellProps {
+interface CellAttributeProps {
 	cell: CellState;
 }
 
-export default ( props: CellProps ) => {
+interface CellStateProps {
+	fields: Map<number, FieldState>;
+	nodes: Map<number, NodeState>;
+}
+
+type CellProps = CellAttributeProps & CellStateProps;
+
+const Cell = ( props: CellProps ) => {
 	const style = {
 		left: props.cell.start.x,
 		top: props.cell.start.y,
@@ -16,7 +24,14 @@ export default ( props: CellProps ) => {
 		bottom: `calc( 100% - ${ props.cell.end.y }px )`,
 	};
 
+	const field = props.fields.get( props.cell.field );
+	const node = field && props.nodes.get( field.resultNode );
+
 	return (
-		<div className="cell" style={ style }>&nbsp;</div>
+		<div className="cell" style={ style }>{ node && node.outputValue }</div>
 	);
 };
+
+export default reduxConnect<{}, {}, CellAttributeProps>(
+	({ fields, nodes }) => ({ fields, nodes }),
+)( Cell );
