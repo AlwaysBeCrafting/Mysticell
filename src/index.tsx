20e3277492bf145f52afb8bee79d65f577fe912a
Alexander Prime <rainesabaal@gmail.com>
2017-04-20 02:55:21 -0700

    Supply type argument to createStore()

--- src/index.tsx
+++ src/index.tsx
@@ -7,20 +7,23 @@ import { createStore } from 'redux';
 import { Editor } from 'components/pages';
 
 import reducer from 'data';
+import { AppState } from 'data';
 import { loadDocument } from 'data/Document/actions';
 
 import 'common/styles/fonts.scss';
 import 'common/styles/normalize.scss';
 
-const store = createStore(
+
+const store = createStore<AppState>(
 	reducer,
 	// tslint:disable-next-line:no-string-literal
 	window['__REDUX_DEVTOOLS_EXTENSION__'] && window['__REDUX_DEVTOOLS_EXTENSION__'](),
 );
 
+store.dispatch( loadDocument( require( 'common/assets/exampleDoc.json' )));
+
 
 const rootElem = document.querySelector( '.root' );
-
 
 const renderRoot = () => {
 	const editor = <Router><Editor /></Router>;
@@ -32,12 +35,8 @@ const renderRoot = () => {
 	);
 };
 
-
 renderRoot();
-
 
 if (( process.env.NODE_ENV === 'development' ) && module.hot ) {
 	module.hot.accept( 'components/pages', renderRoot );
 }
-
-store.dispatch( loadDocument( require( 'common/assets/exampleDoc.json' )));
