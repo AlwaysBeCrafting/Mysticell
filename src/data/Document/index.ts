afecba119c1f756e66a4123566c0d4e4456acd85
Alexander Prime <rainesabaal@gmail.com>
2017-04-20 02:04:34 -0700

    Add default document state

--- src/data/Document/index.ts
+++ src/data/Document/index.ts
@@ -4,7 +4,19 @@ import { Action, ActionTypes } from './actions';
 import { documentJsonToState } from './model';
 
 
-export default ( state: Document, action: Action ): Document => {
+const defaultState: Document = {
+	id: 'DOCUMENT-0000',
+	title: 'Untitled',
+
+	cells: new Map(),
+	sheets: new Map(),
+	nodes: new Map(),
+
+	layout: new Map(),
+};
+
+
+export default ( state: Document = defaultState, action: Action ): Document => {
 	switch ( action.type ) {
 		case ActionTypes.LOAD_DOCUMENT:
 			return documentJsonToState( action.payload.documentJson );
