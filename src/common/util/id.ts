f4913b06d63d73b844cfe6f15485444180db2ad3
Alexander Prime <rainesabaal@gmail.com>
2017-04-22 00:10:40 -0700

    Check shortid validity against the whole ID, not just the name

--- src/common/util/id.ts
+++ src/common/util/id.ts
@@ -6,6 +6,7 @@ const primitiveNamespace = 'PRIM';
 export const generate = ( namespace: string ) => `${namespace}-${shortid.generate()}`;
 export const generateForUser = () => generate( userNamespace );
 export const generateForPrimitive = ( name: string ) => {
-	if ( !shortid.isValid( name )) throw new Error( `Not a valid shortid: ${ name }` );
-	return `${primitiveNamespace}-${name}`;
+	const id = `${primitiveNamespace}-${name}`;
+	if ( !shortid.isValid( id )) { throw new Error( `Not a valid shortid: ${ id }` ); }
+	return id;
 };
