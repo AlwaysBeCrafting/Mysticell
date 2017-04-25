5e96a29f7f0e84b6846cd42dc67e8865b07e187e
Alexander Prime <rainesabaal@gmail.com>
2017-04-24 17:02:19 -0700

    Fix pin panel style header

--- src/components/organisms/GraphEditor/Panel.tsx
+++ src/components/organisms/GraphEditor/Panel.tsx
@@ -11,8 +11,12 @@ interface Props {
 
 const Panel = ({ pinNames, type }: Props ) => {
 	const PinRow = ( type === 'output') ? DestinationPinRow : SourcePinRow;
+
 	return (
 		<div className={ `graphEditor-graph-panel graphEditor-graph-${ type }Panel` }>
+			<div className={ `graphEditor-graph-panel-heading graphEditor-graph-${ type }Panel-heading` }>
+				{ type }
+			</div>
 			{ pinNames.map(( name ) => (
 				<PinRow name={ name } key={ name } />
 			))}
