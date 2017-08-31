58635e095aae2dc0f23cfda18fddf18af45fb875
Alexander <rainesabaal@gmail.com>
2017-08-31 06:09:11 -0700

    Fix broken Card props

--- src/components/atoms/Card/Card.tsx
+++ src/components/atoms/Card/Card.tsx
@@ -4,14 +4,12 @@ import React from "react";
 import "./Card.scss";
 
 
-interface Props {
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
 	className?: string;
-	children?: JSX.Element[] | string;
 }
 
-
-const Card = ({ className, children }: Props) => (
-	<div className={ classNames("card", className) }>
+const Card = ({ className, children, ...attrs }: Props) => (
+	<div className={ classNames("card", className) } { ...attrs }>
 		{ children }
 	</div>
 );
