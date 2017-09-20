9ac11d8e433963979181bdf72558a655ad41e86d
Alexander Prime <rainesabaal@gmail.com>
2017-09-19 17:59:18 -0700

    Create graph resolution function
    Still needs better testing

--- src/data/utils/__tests__/graph.ts
+++ src/data/utils/__tests__/graph.ts
@@ -5,8 +5,14 @@ import exampleDoc from "common/assets/exampleDoc.json";
 import {execFormula} from "../graph";
 
 
-test("graph ", () => {
-	const result = execFormula(exampleDoc)("FORMULA-0000")(PARAMS.number(10))[0];
-	expect(result.type).toBe("number");
-	expect(result.value).toBe(0);
+test("ability score of 10 has modifier 0", () => {
+	const result = execFormula(exampleDoc, "FORMULA-0000", PARAMS.number(10))[0];
+	expect(result).toHaveProperty("type", "number");
+	expect(result).toHaveProperty("value", 0);
+});
+
+test("ability score of 15 has modifier 2", () => {
+	const result = execFormula(exampleDoc, "FORMULA-0000", PARAMS.number(15))[0];
+	expect(result).toHaveProperty("type", "number");
+	expect(result).toHaveProperty("value", 2);
 });
--- src/data/utils/graph.ts
+++ src/data/utils/graph.ts
@@ -1,3 +1,4 @@
+import {Dict} from "common/types";
 import {patchArray} from "common/util";
 
 import {FormulaGraph, Param, PARAMS} from "data/common";
@@ -6,34 +7,123 @@ import {Node} from "data/Node";
 import {PRIMITIVES} from "data/Primitive";
 
 
+interface ExecState {
+	nodeInputs: Dict<number>;
+	nodeUserValues: Dict<Param[]>;
+	readyNodeInputs: Dict<Param[]>;
+	readyNodeOutputs: Dict<Param[]>;
+	compositeGraph: FormulaGraph;
+}
+
+const firstKey = (obj: any): string | undefined => {
+	for (const key in obj) {
+		if (obj.hasOwnProperty(key)) { return key; }
+	}
+	return undefined;
+};
+
 const connectedInputs = (graph: FormulaGraph, nodeId: string) => (
 	graph
 		.filter(edge => edge.target === nodeId)
-		.reduce((prior, edge) => [...prior, edge.edge[1]], [])
+		.reduce((prior, edge) => [...prior, edge.data[1]], [])
 );
 
-const execFormula = (doc: Document) => {
-	const makeFunc = (funcId: string) => (
-		PRIMITIVES[funcId].exec || exec(funcId)
-	);
-	const exec = (formulaId: string) => (...params: Param[]): Param[] => {
-		const resolveInputs = (tgtId: string): Param[] => {
-			if (tgtId === "input") { return params; }
-			const {graph} = doc.formulas[formulaId];
-			const userParams = doc.nodes[tgtId].userValues
-				.map(val => PARAMS.string(val));
-			const inputParams = graph
-				.filter(e => e.target === tgtId)
-				.map(e => [doc.nodes[e.source], e.edge[0]] as [Node, number])
-				.map(src => (
-					makeFunc(src[0].function)(...resolveInputs(src[0].id))[src[1]]
-				));
-			const actualParams = patchArray(userParams, inputParams);
-			return makeFunc(doc.nodes[tgtId].function)(...actualParams);
-		};
-		return resolveInputs("output");
-	};
-	return exec;
+const createExecState = (nodes: Dict<Node>, graph: FormulaGraph, params: Param[]): ExecState => ({
+	nodeInputs: graph.reduce(
+		(prior, edge) => {
+			prior[edge.target] = (prior[edge.target] || 0) + 1;
+			return prior;
+		},
+		{},
+	),
+	nodeUserValues: graph.reduce(
+		(prior, edge) => {
+			if (edge.target === "output") { return prior; }
+			prior[edge.target] = prior[edge.target] ||
+				nodes[edge.target].userValues
+					.map(val => PARAMS.string(val));
+			return prior;
+		},
+		{},
+	),
+	readyNodeInputs: graph.reduce(
+		(prior, edge) => {
+			prior[edge.target] = [];
+			return prior;
+		},
+		{},
+	),
+	readyNodeOutputs: {input: params},
+	compositeGraph: [...graph],
+});
+
+const nodeHasAllInputs = (state: ExecState, nodeId: string): boolean => (
+	state.readyNodeInputs[nodeId].length >= state.nodeInputs[nodeId]
+);
+
+const execIsFinished = (state: ExecState): boolean => (
+	nodeHasAllInputs(state, "output") ||
+	Object.keys(state.readyNodeOutputs).length === 0
+);
+
+const finalizeNode = (state: ExecState, doc: Document, node: Node) => {
+	const primitive = PRIMITIVES[node.function];
+	const formula = doc.formulas[node.function];
+	if (primitive) {
+		state.readyNodeOutputs[node.id] = primitive.exec(
+			...patchArray(state.nodeUserValues[node.id], state.readyNodeInputs[node.id]),
+		);
+	} else {
+		if (formula) {
+			state.compositeGraph = mergeGraphs(
+				state.compositeGraph,
+				doc.formulas[node.function].graph,
+				node.id,
+			);
+		} else {
+			state.readyNodeOutputs[node.id] = state.readyNodeInputs[node.id];
+		}
+	}
+};
+
+const mergeGraphs = (base: FormulaGraph, subgraph: FormulaGraph, nodeId: string): FormulaGraph => {
+	const mergedBase = base
+		.map(edge => {
+			const newEdge = {...edge};
+			if (edge.source === nodeId) { newEdge.source = `${nodeId}-output`; }
+			if (edge.target === nodeId) { newEdge.target = `${nodeId}-input`; }
+			return newEdge;
+		});
+	const mergedSub = subgraph
+		.map(edge => {
+			const newEdge = {...edge};
+			if (edge.source === "input") { newEdge.source = `${nodeId}-input`; }
+			if (edge.target === "output") { newEdge.target = `${nodeId}-output`; }
+			return newEdge;
+		});
+	return {...mergedBase, ...mergedSub};
+};
+
+const execFormula = (doc: Document, formulaId: string, ...params: Param[]) => {
+	const {nodes, formulas} = doc;
+	const {graph} = formulas[formulaId];
+	const state = createExecState(nodes, graph, params);
+
+	while (!execIsFinished(state)) {
+		const currentNodeId = firstKey(state.readyNodeOutputs);
+		if (!currentNodeId) { break; }
+		state.compositeGraph.map(edge => {
+			if (edge.source !== currentNodeId) { return; }
+			state.readyNodeInputs[edge.target][edge.data[1]] = (
+				state.readyNodeOutputs[edge.source][edge.data[0]]
+			);
+			if (edge.target !== "output" && nodeHasAllInputs(state, edge.target)) {
+				finalizeNode(state, doc, doc.nodes[edge.target]);
+			}
+		});
+		delete state.readyNodeOutputs[currentNodeId];
+	}
+	return state.readyNodeInputs.output;
 };
 
 
