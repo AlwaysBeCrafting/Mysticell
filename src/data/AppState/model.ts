bdf35deef3a1b523c2648e329720515f580bdffb
Alexander Prime <rainesabaal@gmail.com>
2017-09-21 16:38:21 -0700

    Add UiState to AppState

--- src/data/AppState/model.ts
+++ src/data/AppState/model.ts
@@ -1,10 +1,12 @@
 import {Document} from "data/Document";
 import {PropertyCache} from "data/PropertyCache";
+import {UiState} from "data/UiState";
 
 
 interface AppState {
 	document: Document;
 	propertyCache: PropertyCache;
+	uiState: UiState;
 }
 
 
--- src/data/AppState/reducer.ts
+++ src/data/AppState/reducer.ts
@@ -1,12 +1,14 @@
 import {combineReducers} from "redux";
 
 import {reducer as document} from "data/Document";
+import {reducer as uiState} from "data/UiState";
 
 import {AppState} from "./model";
 
 
 const reducer = combineReducers<AppState>({
 	document,
+	uiState,
 });
 
 
