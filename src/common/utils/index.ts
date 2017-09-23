9947a42e6ea67dbbadd1fbef5e411641b957808b
Alexander Prime <rainesabaal@gmail.com>
2017-09-22 21:59:54 -0700

    Export new tree utils

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -2,4 +2,4 @@ export {patchArray} from "./array";
 export {formulaLayoutWidth} from "./layout";
 export {generateId} from "./id";
 export {tail} from "./string";
-export {trim} from "./tree";
+export {map, mapBranches, mapLeaves, resolvePath, trim} from "./tree";
