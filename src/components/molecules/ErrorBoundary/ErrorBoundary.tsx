2a477c80388caa1bc0db63d59032142e93c58b01
Alexander Prime <rainesabaal@gmail.com>
2017-10-02 15:09:27 -0700

    Add an ErrorBoundary component
    Takes advantage of new React 16 component error handling via componentDidCatch()

---
+++ src/components/molecules/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import "./ErrorBoundary.scss";
+
+interface Props {
+	children: JSX.Element;
+}
+interface State {
+	error?: Error;
+	errorInfo?: React.ErrorInfo;
+}
+class ErrorBoundary extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+		this.state = {};
+	}
+
+	public componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+		this.setState({
+			error,
+			errorInfo,
+		});
+	}
+
+	public render() {
+		if (this.state.errorInfo) {
+			return (
+				<div className="errorBoundary">
+					<h1 className="errorBoundary-heading">Uh-oh</h1>
+					<div className="errorBoundary-error">
+						{this.state.error && this.state.error.toString()}
+					</div>
+					<div className="errorBoundary-info">
+						{this.state.errorInfo.componentStack}
+					</div>
+				</div>
+			);
+		}
+		return this.props.children;
+	}
+}
+
+
+export { ErrorBoundary };
