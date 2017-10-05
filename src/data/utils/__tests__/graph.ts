9d26a938329ad517bae7710f081dca40c285a8ce
Alexander Prime <rainesabaal@gmail.com>
2017-10-04 18:21:40 -0700

    Try a new graph resolution algorithm
    This builds a list of dependencies from the graph's output, and evaluates them in reverse order

--- src/data/utils/__tests__/graph.ts
+++ src/data/utils/__tests__/graph.ts
@@ -22,6 +22,12 @@ const testDoc: Document = {
 			label: "Adding node B",
 			userValues: ["0", "0"],
 		},
+		"NODE-includeA": {
+			id: "NODE-includeA",
+			function: "FORMULA-addTwice",
+			label: "Add twice",
+			userValues: ["2", "5"],
+		},
 		"NODE-cycleA": {
 			id: "NODE-cycleA",
 			function: "PRIMITIVE-add",
@@ -52,6 +58,20 @@ const testDoc: Document = {
 			],
 			layout: {},
 		},
+		"FORMULA-include": {
+			id: "FORMULA-include",
+			name: "Formula Including Another",
+			inputNames: ["Input number A", "Input number B"],
+			outputNames: ["Output number A", "Output number B"],
+			isProperty: true,
+			graph: [
+				{ source: "input", target: "NODE-includeA", data: [0, 0] },
+				{ source: "input", target: "NODE-includeA", data: [1, 1] },
+				{ source: "NODE-includeA", target: "output", data: [0, 0] },
+				{ source: "NODE-includeA", target: "output", data: [1, 1] },
+			],
+			layout: {},
+		},
 		"FORMULA-cyclic": {
 			id: "FORMULA-cyclic",
 			name: "Cyclic Formula",
@@ -71,15 +91,23 @@ const testDoc: Document = {
 	nav: { value: "root" },
 	propertyInputs: {
 		"FORMULA-addTwice": ["2", "5"],
+		"FORMULA-include": ["2", "5"],
 		"FORMULA-cyclic": ["10"],
 	},
 };
 
 describe("graph resolver", () => {
-	it("resolves a correct graph without errors", async () => {
+	it("resolves a correct graph", async () => {
 		const result = await resolveProperty(testDoc, "FORMULA-addTwice");
 		expect(result)
 			.toHaveLength(testDoc.formulas["FORMULA-addTwice"].outputNames.length);
+		expect(result)
+			.toEqual([PARAMS.number(7), PARAMS.number(7)]);
+	});
+	it("resolves a correct graph that includes another one", async () => {
+		const result = await resolveProperty(testDoc, "FORMULA-include");
+		expect(result)
+			.toHaveLength(testDoc.formulas["FORMULA-include"].outputNames.length);
 		expect(result)
 			.toEqual([PARAMS.number(7), PARAMS.number(7)]);
 	});
--- src/data/utils/graph.ts
+++ src/data/utils/graph.ts
@@ -1,26 +1,192 @@
 import { Dict } from "common/types";
-import { patchArray } from "common/utils";
 
 import { FormulaGraph, Param, PARAMS } from "data/common";
 import { Document } from "data/Document";
-import { Node } from "data/Node";
 import { PRIMITIVES } from "data/Primitive";
 
 
-interface ExecState {
-	nodeInputs: Map<string, number>;
-	nodeUserValues: Map<string, Param[]>;
-	readyNodeInputs: Map<string, Param[]>;
-	readyNodeOutputs: Map<string, Param[]>;
-	compositeGraph: FormulaGraph;
-}
+const resolveProperty = async (doc: Document, propertyId: string): Promise<Param[]> => {
+	const { nodes } = doc;
+	const graph = doc.formulas[propertyId].graph;
+	const dependencyQueue = ["output"];
+	const resolutionQueue: string[] = [];
+	const visited: Set<string> = new Set();
+	const outputParams: Dict<Param[]> = {};
 
-const firstKey = (obj: any): string | undefined => {
-	for (const key in obj) {
-		if (obj.hasOwnProperty(key)) { return key; }
+	while (dependencyQueue.length > 0) {
+		const nodeId = dependencyQueue.shift() as string;
+		if (visited.has(nodeId)) {
+			return Array(doc.formulas[propertyId].outputNames.length)
+				.fill(PARAMS.error(
+					"FUNC",
+					`"${doc.formulas[propertyId].name}" contains a cycle`,
+				));
+		}
+		const bumpedIndices = dependencyQueue
+			.map((target, i) => target === nodeId ? i : -1)
+			.filter(i => i >= 0);
+		const deps = graph
+			.filter(edge => edge.target === nodeId)
+			.map(edge => edge.source);
+		multiSplice(dependencyQueue, bumpedIndices);
+		dependencyQueue.push(...deps);
+		resolutionQueue.unshift(nodeId);
+		if (!dependencyQueue.includes(nodeId)) { visited.add(nodeId); }
 	}
-	return undefined;
+	visited.clear();
+	while (resolutionQueue.length > 0) {
+		const nodeId = resolutionQueue.shift() as string;
+		if (!visited.has(nodeId)) {
+			switch (nodeId) {
+				case "input": {
+					outputParams[nodeId] = doc.propertyInputs[propertyId]
+						.map(str => PARAMS.string(str));
+					break;
+				}
+				case "output": {
+					return getDependedParams(graph, outputParams, "output");
+				}
+				default: {
+					const dependedParams = getDependedParams(graph, outputParams, nodeId);
+					const primitive = PRIMITIVES[nodes[nodeId].function];
+					const params = primitive.exec(...dependedParams);
+					outputParams[nodeId] = params;
+					break;
+				}
+			}
+			visited.add(nodeId);
+		}
+	}
+	return Array(doc.formulas[propertyId].outputNames.length)
+		.fill(PARAMS.error(
+			"FUNC",
+			`Didn't resolve property "${doc.formulas[propertyId].name}" correctly`,
+		));
 };
+
+const getDependedParams = (graph: FormulaGraph, outputParams: Dict<Param[]>, nodeId: string) => {
+	const params: Param[] = [];
+	graph
+		.filter(edge => edge.target === nodeId)
+		.forEach(edge => {
+			params[edge.data[1]] = outputParams[edge.source][edge.data[0]];
+		});
+	return params;
+};
+
+const multiSplice = <T>(array: T[], indices: number[]) => {
+	indices
+		.sort((a, b) => a - b)
+		.forEach((index, i) => array.splice(index - i, 1));
+};
+
+
+// class resolveProperty {
+// 	private doc: Document;
+// 	private propertyId: string;
+// 	private nodeInputs: Map<string, number>;
+// 	private nodeUserValues: Map<string, Param[]>;
+// 	private readyNodeInputs: Map<string, Param[]>;
+// 	private readyNodeOutputs: Map<string, Param[]>;
+// 	private compositeGraph: FormulaGraph;
+
+// 	constructor(doc: Document, propertyId: string) {
+// 		this.doc = doc;
+// 		this.propertyId = propertyId;
+// 		const { nodes, formulas } = doc;
+// 		const property = formulas[propertyId];
+// 		const graph = property.graph;
+// 		const params = doc.propertyInputs[propertyId].map(input => PARAMS.string(input));
+// 		this.nodeInputs = graph.reduce(
+// 			(prior: Map<string, number>, edge) => {
+// 				prior.set(edge.target, (prior.get(edge.target) || 0) + 1);
+// 				return prior;
+// 			},
+// 			new Map(),
+// 		);
+// 		this.nodeUserValues = graph.reduce(
+// 			(prior: Map<string, Param[]>, edge) => (
+// 				edge.target === "output" || prior.has(edge.target)
+// 					? prior
+// 					: prior.set(
+// 						edge.target,
+// 						nodes[edge.target].userValues
+// 							.map(val => PARAMS.string(val)),
+// 					)
+// 			),
+// 			new Map(),
+// 		);
+// 		this.readyNodeInputs = graph.reduce(
+// 			(prior: Map<string, Param[]>, edge) => prior.set(edge.target, []),
+// 			new Map(),
+// 		);
+// 		this.readyNodeOutputs = new Map().set("input", params);
+// 		this.compositeGraph = [...graph];
+// 	}
+
+// 	public async resolve() {
+// 		const { formulas, nodes } = this.doc;
+// 		const formula = formulas[this.propertyId];
+// 		if (!formula.isProperty) {
+// 			throw new Error("Cannot resolveProperty() with a non-property function");
+// 		}
+
+// 		while (!this.isDone()) {
+// 			const currentNodeId = this.readyNodeOutputs.keys().next().value;
+// 			if (!currentNodeId) { break; }
+// 			this.compositeGraph.forEach(edge => {
+// 				if (edge.source !== currentNodeId) { return; }
+// 				const {source, target} = edge;
+// 				const [srcIndex, tgtIndex] = edge.data;
+// 				const outputs = this.readyNodeOutputs.get(source) || [];
+// 				const inputs = this.readyNodeInputs.get(target) || [];
+// 				inputs[tgtIndex] = outputs[srcIndex];
+// 				this.readyNodeInputs.set(target, inputs);
+// 				if (edge.target !== "output" && this.nodeHasAllInputs(edge.target)) {
+// 					const node = nodes[edge.target];
+// 					if (PRIMITIVES[node.function]) {
+// 						this.readyNodeOutputs.set(
+// 							node.id,
+// 							PRIMITIVES[node.function].exec(...patchArray(
+// 								this.nodeUserValues.get(node.id) || [],
+// 								this.readyNodeInputs.get(node.id) || [],
+// 							)),
+// 						);
+// 					} else if (this.doc.formulas[node.function]) {
+// 						this.compositeGraph = mergeGraphs(
+// 							this.compositeGraph,
+// 							this.doc.formulas[node.function].graph,
+// 							node.id,
+// 						);
+// 					} else {
+// 						this.readyNodeOutputs.set(
+// 							node.id,
+// 							this.readyNodeInputs.get(node.id) || [],
+// 						);
+// 					}
+// 				}
+// 			});
+// 			this.readyNodeOutputs.delete(currentNodeId);
+// 		}
+// 		if ((this.readyNodeInputs.get("output") || []).length === this.nodeInputs.get("output") || 0) {
+// 			return this.readyNodeInputs.get("output") || [];
+// 		} else {
+// 			return Array(formula.outputNames.length)
+// 				.fill(PARAMS.error("FUNC", "Could not resolve function"));
+// 		}
+// 	}
+
+// 	private nodeHasAllInputs(nodeId: string): boolean {
+// 		return (
+// 			(this.readyNodeInputs.get(nodeId) || []).length
+// 				>= (this.nodeInputs.get(nodeId) || 0)
+// 		);
+// 	}
+
+// 	private isDone() {
+// 		return this.nodeHasAllInputs("output") || this.readyNodeOutputs.size === 0;
+// 	}
+// }
 
 const connectedInputs = (graph: FormulaGraph, nodeId: string) => (
 	graph
@@ -28,119 +194,23 @@ const connectedInputs = (graph: FormulaGraph, nodeId: string) => (
 		.reduce((prior, edge) => [...prior, edge.data[1]], [])
 );
 
-const createExecState = (nodes: Dict<Node>, graph: FormulaGraph, params: Param[]): ExecState => ({
-	nodeInputs: graph.reduce(
-		(prior: Map<string, number>, edge) => {
-			prior.set(edge.target, (prior.get(edge.target) || 0) + 1);
-			return prior;
-		},
-		new Map(),
-	),
-	nodeUserValues: graph.reduce(
-		(prior: Map<string, Param[]>, edge) => (
-			edge.target === "output" || prior.has(edge.target)
-				? prior
-				: prior.set(
-					edge.target,
-					nodes[edge.target].userValues
-						.map(val => PARAMS.string(val)),
-				)
-		),
-		new Map(),
-	),
-	readyNodeInputs: graph.reduce(
-		(prior: Map<string, Param[]>, edge) => prior.set(edge.target, []),
-		new Map(),
-	),
-	readyNodeOutputs: new Map().set("input", params),
-	compositeGraph: [...graph],
-});
-
-const nodeHasAllInputs = (state: ExecState, nodeId: string): boolean => (
-	(state.readyNodeInputs.get(nodeId) || []).length >= (state.nodeInputs.get(nodeId) || 0)
-);
-
-const execIsFinished = (state: ExecState): boolean => (
-	nodeHasAllInputs(state, "output") ||
-		Object.keys(state.readyNodeOutputs).length === 0
-);
-
-const finalizeNode = (state: ExecState, doc: Document, node: Node) => {
-	const primitive = PRIMITIVES[node.function];
-	const formula = doc.formulas[node.function];
-	if (primitive) {
-		state.readyNodeOutputs.set(
-			node.id,
-			primitive.exec(...patchArray(
-				state.nodeUserValues.get(node.id) || [],
-				state.readyNodeInputs.get(node.id) || [],
-			)),
-		);
-	} else {
-		if (formula) {
-			state.compositeGraph = mergeGraphs(
-				state.compositeGraph,
-				doc.formulas[node.function].graph,
-				node.id,
-			);
-		} else {
-			state.readyNodeOutputs.set(node.id, state.readyNodeInputs.get(node.id) || []);
-		}
-	}
-};
-
-const mergeGraphs = (base: FormulaGraph, subgraph: FormulaGraph, nodeId: string): FormulaGraph => {
-	const mergedBase = base
-		.map(edge => {
-			const newEdge = {...edge};
-			if (edge.source === nodeId) { newEdge.source = `${nodeId}-output`; }
-			if (edge.target === nodeId) { newEdge.target = `${nodeId}-input`; }
-			return newEdge;
-		});
-	const mergedSub = subgraph
-		.map(edge => {
-			const newEdge = {...edge};
-			if (edge.source === "input") { newEdge.source = `${nodeId}-input`; }
-			if (edge.target === "output") { newEdge.target = `${nodeId}-output`; }
-			return newEdge;
-		});
-	return {...mergedBase, ...mergedSub};
-};
-
-const resolveProperty = async (doc: Document, propertyId: string): Promise<Param[]> => {
-	const {nodes, formulas, propertyInputs} = doc;
-	const params = propertyInputs[propertyId].map(input => PARAMS.string(input));
-	const formula = formulas[propertyId];
-	if (!formula.isProperty) {
-		throw new Error("Cannot resolveProperty() with a non-property function");
-	}
-	const {graph} = formula;
-	const state = createExecState(nodes, graph, params);
-
-	while (!execIsFinished(state)) {
-		const currentNodeId = firstKey(state.readyNodeOutputs);
-		if (!currentNodeId) { break; }
-		state.compositeGraph.map(edge => {
-			if (edge.source !== currentNodeId) { return; }
-			const {source, target} = edge;
-			const [srcIndex, tgtIndex] = edge.data;
-			const outputs = state.readyNodeOutputs.get(source) || [];
-			const inputs = state.readyNodeInputs.get(target) || [];
-			inputs[tgtIndex] = outputs[srcIndex];
-			state.readyNodeInputs.set(target, inputs);
-			if (edge.target !== "output" && nodeHasAllInputs(state, edge.target)) {
-				finalizeNode(state, doc, doc.nodes[edge.target]);
-			}
-		});
-		state.readyNodeOutputs.delete(currentNodeId);
-	}
-	if ((state.readyNodeInputs.get("output") || []).length === state.nodeInputs.get("output") || 0) {
-		return state.readyNodeInputs.get("output") || [];
-	} else {
-		return Array(formula.outputNames.length)
-			.fill(PARAMS.error("FUNC", "Could not resolve function"));
-	}
-};
+// const mergeGraphs = (base: FormulaGraph, subgraph: FormulaGraph, nodeId: string): FormulaGraph => {
+// 	const mergedBase = base
+// 		.map(edge => {
+// 			const newEdge = {...edge};
+// 			if (edge.source === nodeId) { newEdge.source = `${nodeId}-output`; }
+// 			if (edge.target === nodeId) { newEdge.target = `${nodeId}-input`; }
+// 			return newEdge;
+// 		});
+// 	const mergedSub = subgraph
+// 		.map(edge => {
+// 			const newEdge = {...edge};
+// 			if (edge.source === "input") { newEdge.source = `${nodeId}-input`; }
+// 			if (edge.target === "output") { newEdge.target = `${nodeId}-output`; }
+// 			return newEdge;
+// 		});
+// 	return [...mergedBase, ...mergedSub];
+// };
 
 
 export { connectedInputs, resolveProperty };
