cb698559d46d2b3288d7a009004c9e9629dd9fdd
Alexander Prime <rainesabaal@gmail.com>
2017-10-16 14:29:29 -0700

    Fix errors when switching graphs

--- src/components/organisms/GraphView/WireLayer.tsx
+++ src/components/organisms/GraphView/WireLayer.tsx
@@ -7,7 +7,7 @@ import { graphLayoutWidth } from "common/utils";
 import { Wire } from "components/atoms";
 
 import { PRIMITIVES } from "data/common";
-import { Edge, GraphNode, InnerNode } from "data/Graph";
+import { Edge, InnerNode } from "data/Graph";
 import { GraphNodePrototype, NodePrototype } from "data/NodePrototype";
 
 const nodeHeaderRows = 1;
@@ -25,55 +25,55 @@ class WireLayer extends React.PureComponent<Props> {
     const { prototype, className } = this.props;
     return (
       <svg className={classNames("wireLayer", className)}>
-        {Object.values(prototype.graph).map(this.renderNodeWires)}
+        {Object.values(prototype.graph).map(node =>
+          node.edges.map(this.renderWire(node.id)),
+        )}
       </svg>
     );
   }
 
-  private renderNodeWires = (node: GraphNode) =>
-    node.edges.map(this.renderWire(node.id));
-
-  private renderWire = (source: string) => (edge: Edge) => {
+  private renderWire(source: string) {
     const { graph, layout } = this.props.prototype;
-    const { target } = edge;
-    const [srcIndex, tgtIndex] = edge.data;
+    return (edge: Edge) => {
+      const { target } = edge;
+      const [srcIndex, tgtIndex] = edge.data;
+      const srcPos: [number, number] = [0, 0];
+      const dstPos: [number, number] = [0, 0];
 
-    const srcPos: [number, number] = [0, 0];
-    const dstPos: [number, number] = [0, 0];
+      if (source === "input") {
+        srcPos[1] += panelHeaderRows;
+      } else {
+        srcPos[0] += layout[source][0];
+        srcPos[1] += layout[source][1];
+        srcPos[0] += nodeWidth;
+        srcPos[1] += nodeHeaderRows;
+      }
 
-    if (source === "input") {
-      srcPos[1] += panelHeaderRows;
-    } else {
-      srcPos[0] += layout[source][0];
-      srcPos[1] += layout[source][1];
-      srcPos[0] += nodeWidth;
-      srcPos[1] += nodeHeaderRows;
-    }
+      if (target === "output") {
+        dstPos[0] = graphLayoutWidth(layout);
+        dstPos[1] += panelHeaderRows;
+      } else {
+        dstPos[0] = layout[target][0];
+        dstPos[1] = layout[target][1];
+        dstPos[1] += nodeHeaderRows;
+        const tgtNode = graph[target] as InnerNode;
+        dstPos[1] += (PRIMITIVES[tgtNode.prototype] ||
+          this.props.nodePrototypes[tgtNode.prototype]
+        ).outputNames.length;
+      }
 
-    if (target === "output") {
-      dstPos[0] = graphLayoutWidth(layout);
-      dstPos[1] += panelHeaderRows;
-    } else {
-      dstPos[0] = layout[target][0];
-      dstPos[1] = layout[target][1];
-      dstPos[1] += nodeHeaderRows;
-      const tgtNode = graph[target] as InnerNode;
-      dstPos[1] += (PRIMITIVES[tgtNode.prototype] ||
-        this.props.nodePrototypes[tgtNode.prototype]
-      ).outputNames.length;
-    }
+      srcPos[1] += srcIndex;
+      dstPos[1] += tgtIndex;
 
-    srcPos[1] += srcIndex;
-    dstPos[1] += tgtIndex;
-
-    return (
-      <Wire
-        srcPos={srcPos}
-        dstPos={dstPos}
-        key={`${source}@${srcIndex}-${target}@${tgtIndex}`}
-      />
-    );
-  };
+      return (
+        <Wire
+          srcPos={srcPos}
+          dstPos={dstPos}
+          key={`${source}@${srcIndex}-${target}@${tgtIndex}`}
+        />
+      );
+    };
+  }
 }
 
 export { WireLayer };
