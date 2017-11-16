22424e42a3501d742d42689d475888072dacf081
Alexander Prime <rainesabaal@gmail.com>
2017-11-16 14:11:00 -0700

    Update property outputs when an input is changed

--- src/data/CardTemplate/Graph/Property/epic.ts
+++ src/data/CardTemplate/Graph/Property/epic.ts
@@ -3,22 +3,42 @@ import { ActionsObservable, combineEpics } from "redux-observable";
 
 import { Observable } from "common/rxjs";
 
-import { Action, ActionTypes, setInputValue } from "./actions";
+import { AppState } from "data/AppState";
+import { resolveGraph } from "data/utils";
 
-const changeInputValueEpic = (
+import { Action, ActionTypes, setInputValue, setOutputValues } from "./actions";
+
+const setInputValueEpic = (
   action$: ActionsObservable<Action>,
 ): Observable<Redux.Action> =>
   action$
     .ofType(ActionTypes.SET_INPUT_VALUE_ASYNC)
     .debounceTime(300)
-    .map((action: Action) => {
+    .mergeMap((action: Action) => {
       if (action.type !== ActionTypes.SET_INPUT_VALUE_ASYNC) {
-        return { type: "none" };
+        return [];
       }
       const { propertyId, node, value } = action.payload;
-      return setInputValue(propertyId, node, value);
+      return [setInputValue(propertyId, node, value)];
     });
 
-const epic = combineEpics(changeInputValueEpic);
+const setOutputValuesEpic = (
+  action$: ActionsObservable<Action>,
+  store: Redux.Store<AppState>,
+): Observable<Redux.Action> =>
+  action$.ofType(ActionTypes.SET_INPUT_VALUE).mergeMap((action: Action) => {
+    if (action.type !== ActionTypes.SET_INPUT_VALUE) {
+      return [];
+    }
+    const { propertyId } = action.payload;
+    const { palette } = store.getState().document;
+    return [
+      setOutputValues(
+        propertyId,
+        resolveGraph(palette.getProperty(propertyId)!, palette),
+      ),
+    ];
+  });
+const epic = combineEpics(setInputValueEpic, setOutputValuesEpic);
 
 export { epic };
