4d011332cf9d9311629cc1a7203a6581124b2ac1
Alexander Peters <rainesabaal@gmail.com>
2018-01-10 16:19:23 -0700

    Parameterize dev server port and URL

--- config/paths.js
+++ config/paths.js
@@ -1,10 +1,10 @@
-const path = require('path');
-const appRootPath = require('app-root-path').path;
+const path = require("path");
+const appRootPath = require("app-root-path").path;
 
 module.exports = {
-	appRoot: appRootPath,
-	appSrc: path.resolve(appRootPath, './src'),
-	appIndex: path.resolve(appRootPath, './src/index.tsx'),
-	appDist: path.resolve(appRootPath, './dist'),
-	appHtml: path.resolve(appRootPath, './public/index.html'),
+  appRoot: appRootPath,
+  appSrc: path.resolve(appRootPath, "./src"),
+  appIndex: path.resolve(appRootPath, "./src/index.tsx"),
+  appDist: path.resolve(appRootPath, "./dist"),
+  appHtml: path.resolve(appRootPath, "./public/index.html"),
 };
