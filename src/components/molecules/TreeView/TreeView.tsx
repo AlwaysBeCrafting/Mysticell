19dff351fc845a4bbb9d08bfd458514541023edf
Alexander Peters <rainesabaal@gmail.com>
2018-03-30 14:36:53 -0700

    Improve and simplify TreeView interface
    No special `Tree` type is needed now. Users supply `render()`, `getChildren()`, and `getKey()` functions, which
    `TreeView` uses to build and compose the individual tree items.

--- src/components/molecules/TreeView/TreeView.tsx
+++ src/components/molecules/TreeView/TreeView.tsx
@@ -1,81 +1,47 @@
 import classnames from "classnames";
-import { List } from "immutable";
+import { Collection } from "immutable";
 import React from "react";
-
-import { Tree, TreeKey } from "common/types";
 
 import "./TreeView.scss";
 
-interface Props<K extends TreeKey, V> {
+interface Props<I> {
   className?: string;
-  tree: Tree<K, V>;
-  renderItem: (item: V, path: List<K>) => JSX.Element | null;
-  getItemKey: (item: V, path: List<K>) => string;
-  shouldRenderChildren?: (item: V, path: List<K>) => boolean;
+  render: (item: I) => JSX.Element | null;
+  getChildren: (item?: I) => Collection.Indexed<I>;
+  getKey: (item: I) => string;
 }
 
-class TreeView<K extends TreeKey, V> extends React.PureComponent<Props<K, V>> {
+class TreeView<I> extends React.PureComponent<Props<I>> {
   render() {
-    const { className, tree, getItemKey, shouldRenderChildren } = this.props;
-    const shouldRecurse =
-      !shouldRenderChildren || shouldRenderChildren(tree.value, List());
+    const { className, getChildren, getKey } = this.props;
     return (
       <ul className={classnames("treeView", className)}>
-        {shouldRecurse &&
-          tree.children
-            .toSeq()
-            .map((child, key) => (
-              <Item
-                {...this.props}
-                key={getItemKey(child.value, List.of(key))}
-                path={List.of(key)}
-                tree={child}
-              />
-            ))
-            .toIndexedSeq()}
+        {getChildren().map(item => (
+          <Item item={item} {...this.props} key={getKey(item)} />
+        ))}
       </ul>
     );
   }
 }
 
-interface ItemProps<K extends TreeKey, V> extends Props<K, V> {
-  path: List<K>;
+interface ItemProps<I> extends Props<I> {
+  item: I;
 }
 
-class Item<K extends TreeKey, V> extends React.PureComponent<ItemProps<K, V>> {
+class Item<I> extends React.PureComponent<ItemProps<I>> {
   render(): JSX.Element {
-    const {
-      path,
-      tree,
-      renderItem,
-      getItemKey,
-      shouldRenderChildren,
-    } = this.props;
-    const shouldRecurse =
-      !shouldRenderChildren || shouldRenderChildren(tree.value, path);
-    const childrenElem = shouldRecurse && (
+    const { item, render, getChildren, getKey } = this.props;
+    const children = getChildren(item);
+    const childrenElem = children.count() > 0 && (
       <ul className="treeView-item-children">
-        {tree.children
-          .toSeq()
-          .map((child, key) => {
-            const childPath = path.push(key);
-            return (
-              <Item
-                {...this.props}
-                key={getItemKey(child.value, childPath)}
-                path={childPath}
-                tree={child}
-              />
-            );
-          })
-          .toIndexedSeq()}
+        {children.map(child => (
+          <Item item={child} {...this.props} key={getKey(child)} />
+        ))}
       </ul>
     );
     return (
       <li className="treeView-item">
-        <span className="treeView-item-body">
-          {renderItem(tree.value, path)}
-        </span>
+        <span className="treeView-item-body">{render(item)}</span>
         {childrenElem}
       </li>
     );
