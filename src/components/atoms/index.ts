be945c9ecff0818765164632cb988aaf94eea3fa
Alexander Peters <rainesabaal@gmail.com>
2018-04-10 15:46:32 -0700

    Remove broken export

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,4 @@
 export { CellView } from "./CellView";
-export { Pin } from "./Pin";
 export { TerminalView } from "./TerminalView";
 export { ToolButton } from "./ToolButton";
 export { Wire } from "./Wire";
