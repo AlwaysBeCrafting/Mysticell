a222216dc7dea73afdd47384120962a0c86deaf7
Alexander Peters <alexander.n.peters@gmail.com>
2018-09-18 21:09:50 -0700

    Set up postgres and knex config

---
+++ api/knexfile.ts
@@ -0,0 +1,17 @@
+export = {
+  client: "postgresql",
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  },
+  migrations: {
+    directory: "./migrations",
+    tableName: "knex_migrations",
+  },
+  seeds: {
+    directory: "./seeds",
+  },
+};
