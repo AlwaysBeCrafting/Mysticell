85cf9fce7b5fc9da9fd9ec003a4f5a26e13042dc
Alexander Peters <alexander.n.peters@gmail.com>
2019-03-23 12:47:34 -0700

    Remove "Directory" frontend data type
    Decided we're gonna handle source hierarchy at render time

--- web/src/data/App/actions.ts
+++ web/src/data/App/actions.ts
@@ -1,5 +1,4 @@
 import { Action as CellAction } from "data/Cell";
-import { Action as DirectoryAction } from "data/Directory";
 import { Action as DocumentAction } from "data/Document";
 import { Action as NodeAction } from "data/Node";
 import { Action as SheetAction } from "data/Sheet";
@@ -8,7 +7,6 @@ import { Action as WireAction } from "data/Wire";
 
 type Action =
   | CellAction
-  | DirectoryAction
   | DocumentAction
   | NodeAction
   | SheetAction
